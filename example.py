a488867646f624bbe35c57efc1baea71e659d538
Michael Willame <michael.willame@qarnot-computing.com>
2014-12-15 15:04:39 +0100

    added configuration file

--- example.py
+++ example.py
@@ -1,19 +1,22 @@
 #! /usr/bin/python
 """example script for api usage """
 
+from __future__ import print_function
+
 import qapy
+import warnings
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 if __name__ == "__main__":
-    try:
-        q = qapy.QConnection('https://localhost', 'second')
-        task = q.create_task("example task", "python", 3)
+    warnings.simplefilter("ignore", InsecureRequestWarning)
+    #remporary workaround for certificate issue
+    q = qapy.QConnection('example/qarnot.conf')
+    with q.create_task("example task", "python", 3) as task:
         task.resources.add_file("example/script.py")
         task.constants['PYTHON_SCRIPT'] = "script.py"
         task.submit()
         task.wait()
-        print(task.stdout)
+        print(task.stdout, end='')
         for fInfo in task.results.list_files():
             with open(task.results[fInfo]) as f:
                 print(f.read())
-    finally:
-        task.delete()
