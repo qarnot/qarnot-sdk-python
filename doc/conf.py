62705390a9fa2a18dac9ca548a28270966791380
Michael Willame <michael.willame@qarnot-computing.com>
2015-01-19 14:59:04 +0100

    added upload mode test

--- doc/conf.py
+++ doc/conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'qapy'
-copyright = u'2014, Author'
+copyright = u'2014, Qarnot Computing'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
