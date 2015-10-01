868855a2d119dcb2b696a29585d9a31e979ae57a
Clément Pellegrini <clement.pellegrini@qarnot-computing.com>
2015-10-01 09:32:49 +0200

    update main example file

--- example.py
+++ example.py
@@ -9,20 +9,30 @@ from qapy.disk import QUploadMode
 from os import walk
 from os.path import join
 
+def main():
+    """main function"""
+    qarnot = qapy.QApy('example/qarnot.conf')
+    with qarnot.create_task("example task", "python", 3) as task:
+        task.resources.add_file("example/script_verbose.py",
+                                mode=QUploadMode.background)
+        task.constants['PYTHON_SCRIPT'] = "script_verbose.py"
+
+        print("Submit task")
+        task.submit()
+
+        print ("Wait task")
+        while not task.wait(10):
+            print (task.fresh_stdout())
+
+            outdir = tempfile.mkdtemp()
+            print ("Download resultuts to " + outdir)
+            task.download_results(outdir)
+            for dirname, _, files in walk(outdir):
+                for filename in files:
+                    with open(join(dirname, filename)) as file_:
+                        print(file_.read())
+
+
 if __name__ == "__main__":
-    q = qapy.QApy('example/qarnot.conf')
-    task = q.create_task("example task", "python", 3)
-    task.resources.add_file("example/script_verbose.py",
-                            mode=QUploadMode.background)
-    task.constants['PYTHON_SCRIPT'] = "script_verbose.py"
-    out = tempfile.mkdtemp()
-    task.submit(out)
-    print ("out " + out)
-    while not task.wait(10):
-        print ("...")
-        print (task.fresh_stdout())
-    for dirname, dirs, files in walk(out):
-        for filename in files:
-            with open(join(dirname,filename)) as f:
-                print(f.read())
-    task.delete()
+    main()
+
